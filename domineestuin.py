c7c68c5d6e6dad7c310968288104bd89123cb69e
jan438 <jan438@users.noreply.github.com>
2026-09-12 14:22:42 +0200

    dominees added

--- domineestuin.py
+++ domineestuin.py
@@ -8,6 +8,19 @@ from datetime import datetime, date, timedelta
 from reportlab.pdfgen import canvas
 from reportlab.lib.colors import HexColor
 from reportlab.lib.units import inch, mm
+from svglib.svglib import svg2rlg, load_svg_file, SvgRenderer
+from reportlab.graphics import renderPDF
+
+def scaleSVG(svgfile, scaling_factor):
+    svg_root = load_svg_file(svgfile)
+    svgRenderer = SvgRenderer(svgfile)
+    drawing = svgRenderer.render(svg_root)
+    scaling_x = scaling_factor
+    scaling_y = scaling_factor
+    drawing.width = drawing.minWidth() * scaling_x
+    drawing.height = drawing.height * scaling_y
+    drawing.scale(scaling_x, scaling_y)
+    return drawing
 
 if sys.platform[0] == 'l':
     path = "/home/jan/git/ZaanseSchans"
@@ -15,5 +28,6 @@ if sys.platform[0] == 'w':
     path = "C:/Users/janbo/OneDrive/Documents/GitHub/ZaanseSchans"
 os.chdir(path)
 my_canvas = canvas.Canvas("PDF/ZaanseSchans.pdf")
+renderPDF.draw(scaleSVG("SVG/orgelmakersabw.svg", 5.0), my_canvas, 100.0, 100.0)
 my_canvas.save()
 key = input("Wait")
